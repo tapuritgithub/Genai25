a2009f1f6ddcfac6f52f176f59b6a3f8935f67da
TAPASWINI DAS <tapaswinidas0702@gmail.com>
2025-06-18 20:49:33 +0530

    Completed

---
+++ Assignment1/src/que4.kt
@@ -0,0 +1,17 @@
+package Day3
+
+fun main(){
+    var size1="small"
+    var size2="large"
+    println("before swapping")
+    println("size1=$size1")
+    println("size2=$size2")
+
+    var temp=size1
+    size1=size2
+    size2=temp
+    println("after swapping")
+    println("size1=$size1")
+    println("size2=$size2")
+
+}
