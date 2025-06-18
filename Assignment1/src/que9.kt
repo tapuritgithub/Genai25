033362cc0b885a38ca1c4ab99d254e53f3151755
TAPASWINI DAS <tapaswinidas0702@gmail.com>
2025-06-18 21:02:12 +0530

    Completed

---
+++ Assignment1/src/que9.kt
@@ -0,0 +1,10 @@
+package Day3
+
+fun main(){
+    val age=50
+    if(age>=60){
+        println("age $age is eligible for senior  discount ")
+    }else{
+        println(" Oops! age $age is  not eligible for senior discount")
+    }
+}
