7219a75c5e4785ca5084a8862f45cf6a1f9b0195
TAPASWINI DAS <tapaswinidas0702@gmail.com>
2025-06-20 14:06:53 +0530

    completed

---
+++ Assignment2/Asignment2/src/que15.kt
@@ -0,0 +1,10 @@
+package Day3
+
+fun main(){
+    var stock=10
+    do{
+        println("coffee stock left is $stock")
+        stock--
+    }while(stock>=1)
+    println("Coffee is out of stock")
+}
