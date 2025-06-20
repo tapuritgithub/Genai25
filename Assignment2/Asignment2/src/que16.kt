91007c6d9146240972635ed1236312b57f862a26
TAPASWINI DAS <tapaswinidas0702@gmail.com>
2025-06-20 14:08:45 +0530

    completed

---
+++ Assignment2/Asignment2/src/que16.kt
@@ -0,0 +1,10 @@
+package Day3
+
+fun main(){
+    var name="Espresso"
+    if(name.length>=5){
+        println("$name has mora than 5 letters" )
+    }else{
+        println("coffee name has les than 5 letters")
+    }
+}
